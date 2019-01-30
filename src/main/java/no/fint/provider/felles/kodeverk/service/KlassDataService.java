5fd08f5edf9af7f5db673bc8487920d5d6948e68
Asgeir Nilsen <asgeir@fintlabs.no>
2019-01-30 14:33:35 +0100

    Reverted initialDelay.

--- src/main/java/no/fint/provider/felles/kodeverk/service/KlassDataService.java
+++ src/main/java/no/fint/provider/felles/kodeverk/service/KlassDataService.java
@@ -1,19 +1,18 @@
 package no.fint.provider.felles.kodeverk.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.felles.kodeverk.Fylke;
 import no.fint.model.felles.kodeverk.Kommune;
 import no.fint.provider.felles.kodeverk.client.KlassClient;
 import no.fint.provider.felles.kodeverk.model.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -40,7 +39,7 @@ public class KlassDataService {
 	@Getter
 	private volatile List<Fylke> fylker;
 
-    @Scheduled(initialDelay = 2000, cron = "${fint.adapter.ssb-klass.interval:0 */10 * * * *}")
+    @Scheduled(cron = "${fint.adapter.ssb-klass.interval:0 */10 * * * *}")
 	public void update() {
 		log.info("Fetching classifications from SSB...");
 		kommuner = client.getCodes(kommuneKode, validFrom, validTo).getCodes().stream().map(Mapper::toKommune).collect(Collectors.toList());
