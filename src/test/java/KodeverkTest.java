8684f09cd47b9b7e6b00bcf43f80e967bdb3f09d
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2024-09-24 13:03:41 +0200

    Update and fix bug (#3)
    * Update spring+java+gradle and make it compile
    
    * Change url into https

--- src/test/java/KodeverkTest.java
+++ src/test/java/KodeverkTest.java
@@ -1,41 +1,37 @@
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import no.fint.provider.felles.kodeverk.client.Kodeverk;
 import no.fint.provider.felles.kodeverk.model.Kode;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.util.List;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KodeverkTest {
 
-	@Test
-	public void kjonn() throws JsonParseException, JsonMappingException, IOException {
-		List<Kode> kjonn = new Kodeverk().getKjonn();
-		assertThat(kjonn.size(), is(4));
-	}
+    @Test
+    public void kjonn() {
+        List<Kode> kjonn = new Kodeverk().getKjonn();
+        assertEquals(kjonn.size(), 4);
+    }
 
-	@Test
-	public void land() throws Exception {
-		List<Kode> land = new Kodeverk().getLand();
-		assertThat(land.size(), is(249));
-		assertThat(land.stream().filter(k -> k.getKode().equals("NO")).map(Kode::getNavn).findAny().get(), is("Norge"));
-	}
+    @Test
+    public void land() {
+        List<Kode> land = new Kodeverk().getLand();
+        assertEquals(land.size(), 249);
+        assertEquals(land.stream().filter(k -> k.getKode().equals("NO")).map(Kode::getNavn).findAny().get(), "Norge");
+    }
 
-	@Test
-	public void spraak() throws Exception {
-		List<Kode> sprak = new Kodeverk().getSprak();
-		assertThat(sprak.size(), is(186));
-		assertThat(sprak.stream().filter(k -> k.getKode().equals("no")).map(Kode::getNavn).findAny().get(), is("Norsk"));
-	}
+    @Test
+    public void spraak() {
+        List<Kode> sprak = new Kodeverk().getSprak();
+        assertEquals(sprak.size(), 186);
+        assertEquals(sprak.stream().filter(k -> k.getKode().equals("no")).map(Kode::getNavn).findAny().get(), "Norsk");
+    }
 
-	@Test
-	public void stillingskoder() throws Exception {
-		List<Kode> stillingskoder = new Kodeverk().getStillingskoder();
-		assertThat(stillingskoder.size(), is(91));
-		assertThat(stillingskoder.stream().filter(k -> k.getKode().equals("6014")).map(Kode::getNavn).findAny().get(), is("ARBEIDER"));
-	}
+    @Test
+    public void stillingskoder() {
+        List<Kode> stillingskoder = new Kodeverk().getStillingskoder();
+        assertEquals(stillingskoder.size(), 91);
+        assertEquals(stillingskoder.stream().filter(k -> k.getKode().equals("6014")).map(Kode::getNavn).findAny().get(), "ARBEIDER");
+    }
 }
